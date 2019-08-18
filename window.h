cc0595d7fd3f99559ee7946d5877d65a9461d42d
sdtcsyl <sdtcsyl@163.com>
2019-08-18 19:43:09 +0800

    Add files via upload

--- window.h
+++ window.h
@@ -10,51 +10,93 @@
 #include<winuser.h>
 using namespace std;
 
-// click OPEN window's cancel button
-// the OPEN window cannot display two window.
-int wnd_open_cancel(){
-    HWND wnd, wnd_child;
-    int res_down, res_up;
-    wnd = FindWindowA(NULL,"Open");
-    cout << wnd << endl;
-    wnd_child = GetWindow(wnd, GW_CHILD);
-    cout << wnd_child << endl;
-    wnd_child = FindWindowExA(wnd, wnd_child, NULL, "CANCEL");
-    cout << wnd_child << endl;
-    res_down = SendMessage(wnd_child, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(0, 0));
-    res_up = SendMessage(wnd_child, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(0, 0));
-    cout << res_down << endl << res_up << endl;
-    return res_down;
+// the OPEN window Class
+
+class WND_OPEN
+{
+    private:
+        HWND wnd, wnd_child;
+        int res_down, res_up;
+        BOOL res;
+    public:
+        int click_cancel();
+        int click_ok();
+        BOOL set_text(const char* name);
 }
 
-int wnd_open_ok(){
-    HWND wnd, wnd_child;
-    int res_down, res_up;
-    wnd = FindWindowA(NULL,"Open");
-    cout << wnd << endl;
-    wnd_child = GetWindow(wnd, GW_CHILD);
-    cout << wnd_child << endl;
-    wnd_child = FindWindowExA(wnd, wnd_child, NULL, "OK");
-    cout << wnd_child << endl;
-    res_down = SendMessage(wnd_child, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(0, 0));
-    res_up = SendMessage(wnd_child, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(0, 0));
-    cout << res_down << endl << res_up << endl;
-    return res_down;
+int WND_OPEN::click_cancel()
+{
+    this->wnd = FindWindowA(NULL,"Open");
+    this->wnd_child = GetWindow(this->wnd, GW_CHILD);
+    this->wnd_child = FindWindowExA(this->wnd, this->wnd_child, NULL, "CANCEL");
+    this->res_down = SendMessage(this->wnd_child, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(0, 0));
+    this->res_up = SendMessage(this->wnd_child, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(0, 0));
+    return this->res_down;
 }
 
-BOOL wnd_open_edit(const char* name){
-    HWND wnd, wnd_child;
-    BOOL res;
-
-    wnd = FindWindowA(NULL,"Open");
-    cout << wnd << endl;
-    wnd_child = GetWindow(wnd, GW_CHILD);
-    cout << wnd_child << endl;
-    wnd_child = FindWindowExA(wnd, wnd_child, NULL, "Files of &type:");
-    wnd_child = GetWindow(wnd_child, GW_HWNDPREV);
-    cout << wnd_child << endl;
-    res = SendMessage(wnd_child, WM_SETTEXT, 0, (LPARAM) name) ;
-    
-    cout << res << endl;
-    return res;
+int WND_OPEN::click_ok()
+{
+    this->wnd = FindWindowA(NULL,"Open");
+    this->wnd_child = GetWindow(this->wnd, GW_CHILD);
+    this->wnd_child = FindWindowExA(this->wnd, this->wnd_child, NULL, "OK");
+    this->res_down = SendMessage(this->this->wnd_child, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(0, 0));
+    this->res_up = SendMessage(wnd_child, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(0, 0));
+    return this->res_down;
 }
+
+BOOL WND_OPEN::set_text(const char* name)
+{
+    this->wnd = FindWindowA(NULL,"Open");
+    this->wnd_child = GetWindow(this->wnd, GW_CHILD);
+    this->wnd_child = FindWindowExA(this->wnd, this->wnd_child, NULL, "Files of &type:");
+    this->wnd_child = GetWindow(this->wnd_child, GW_HWNDPREV);
+    this->res = SendMessage(this->wnd_child, WM_SETTEXT, 0, (LPARAM) name);    
+    return this->res;
+}
+
+// the SAVEAS window Class
+
+class WND_SAVEAS
+{
+    private:
+        HWND wnd, wnd_child;
+        int res_down, res_up;
+        BOOL res;
+    public:
+        int click_cancel();
+        int click_save();
+        BOOL set_text(const char* name);
+}
+
+int WND_SAVEAS::click_cancel()
+{
+    this->wnd = FindWindowA(NULL,"Save As");
+    this->wnd_child = GetWindow(this->wnd, GW_CHILD);
+    this->wnd_child = FindWindowExA(this->wnd, this->wnd_child, NULL, "CANCEL");
+    this->res_down = SendMessage(this->wnd_child, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(0, 0));
+    this->res_up = SendMessage(this->wnd_child, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(0, 0));
+    return this->res_down;
+}
+
+int WND_SAVEAS::click_save()
+{
+    this->wnd = FindWindowA(NULL,"Save As");
+    this->wnd_child = GetWindow(this->wnd, GW_CHILD);
+    this->wnd_child = FindWindowExA(this->wnd, this->wnd_child, NULL, "&Save");
+    this->res_down = SendMessage(this->this->wnd_child, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(0, 0));
+    this->res_up = SendMessage(wnd_child, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(0, 0));
+    return this->res_down;
+}
+
+BOOL WND_SAVEAS::set_text(const char* name)
+{
+    this->wnd = FindWindowA(NULL,"Save As");
+    this->wnd_child = GetWindow(this->wnd, GW_CHILD);
+    this->wnd_child = GetWindow(this->wnd_child, GW_CHILD);
+    this->wnd_child = GetWindow(this->wnd_child, GW_CHILD);
+    this->wnd_child = GetWindow(this->wnd_child, GW_CHILD);
+    this->wnd_child = GetWindow(this->wnd_child, GW_CHILD);
+    this->res = SendMessage(this->wnd_child, WM_SETTEXT, 0, (LPARAM) name);    
+    return this->res;
+}
+
